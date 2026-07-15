4ad52c8d3086836d21266da6703c724804cf4e91
hjj <2836453230@qq.com>
2026-07-15 16:49:10 +0800

    新增hello测试接口

--- src/main/java/org/example/camp/CampApplication.java
+++ src/main/java/org/example/camp/CampApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CampApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CampApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CampApplication.class, args);
+    }
 
 }
---
+++ src/main/java/org/example/camp/controller/HelloHJJ.java
@@ -0,0 +1,18 @@
+package org.example.camp.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HelloHJJ {
+
+    @GetMapping("/hello")
+    public String Hello(){
+
+        log.info("HelloHJJ");
+
+        return "Hello HJJ";
+    }
+}
