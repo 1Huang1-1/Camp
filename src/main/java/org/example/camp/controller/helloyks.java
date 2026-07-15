e9c01759d5e6dc545c6b66f42e1a2ed51eda1457
yyyyyks <3364233894@qq.com>
2026-07-15 16:56:51 +0800

    添加helloyks接口

---
+++ src/main/java/org/example/camp/controller/helloyks.java
@@ -0,0 +1,30 @@
+package org.example.camp.controller;
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloyks {
+
+    private static final Logger log = LoggerFactory.getLogger(helloyks.class);
+
+    @GetMapping("/helloyks")
+    public String hello(@RequestParam(defaultValue = "锁哥") String name) {
+        log.info("Received hello request with name: {}", name);
+        String greeting = "Hello, " + name + "！这是我给你搭的 Spring Boot 项目 😄";
+        log.debug("Response: {}", greeting);
+        return greeting;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        log.info("Root endpoint accessed");
+        return "Spring Boot 项目启动成功！试试访问 /hello?name=一一";
+    }
+}
+
